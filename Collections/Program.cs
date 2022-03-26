d12ef1cfe1341418630cfee2363719909c348e8f
Maksim <Min-max2000@mail.ru>
2022-03-26 15:39:19 +0300

    Добавил задания по обобщениям и коллекциям

---
+++ Collections/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Collections
+{
+    class Entity {
+        public int Id;
+        public int ParentId;
+        public string Name;
+
+        public Entity(int Id, int ParentId, string Name) {
+            this.Id = Id;
+            this.ParentId = ParentId;
+            this.Name = Name;
+        }
+    }
+    class Program
+    {
+        public static Dictionary<int, List<Entity>> ListToDict(List<Entity> entities)
+        {
+            IEnumerable<Entity> query = entities
+                .OrderBy(x => x.ParentId);
+            Dictionary<int, List<Entity>> peoples = new Dictionary<int, List<Entity>>();
+            foreach (var people in query) {
+                if (peoples.ContainsKey(people.ParentId))
+                    peoples[people.ParentId].Add(people);
+                else
+                {
+                    var entityList = new List<Entity>();
+                    entityList.Add(people);
+                    peoples[people.ParentId] = entityList;
+                }
+            }
+            //Проверка
+            foreach (var people in peoples)
+            {
+                foreach (var el in people.Value)
+                {
+                    Console.WriteLine($"Ключ: {people.Key}, Id: {el.Id}, ParentId: {el.ParentId}, Name: {el.Name}");
+                }
+            }
+            return peoples;
+        }
+        static void Main(string[] args)
+        {
+            var people1 = new Entity(1, 0, "Root entity");
+            var people2 = new Entity(2, 1, "Child of 1 entity");
+            var people3 = new Entity(3, 1, "Child of 1 entity");
+            var people4 = new Entity(4, 2, "Child of 2 entity");
+            var people5 = new Entity(5, 4, "Child of 4 entity");
+
+            List<Entity> entities = new List<Entity>();
+            entities.Add(people1);
+            entities.Add(people2);
+            entities.Add(people3);
+            entities.Add(people4);
+            entities.Add(people5);
+            Program.ListToDict(entities);
+        }
+    }
+}
---
+++ Generalization/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+
+namespace Generalization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileLogger = new LocalFileLogger<int>("text.txt");
+            fileLogger.LogInfo("message1");
+            fileLogger.LogInfo("message2");
+            fileLogger.LogError("message3", new Exception("Ошибочка вышла"));
+
+            var fileLogger2 = new LocalFileLogger<string>("text.txt");
+            fileLogger2.LogInfo("message4");
+            fileLogger2.LogInfo("message5");
+            fileLogger2.LogError("message6", new Exception("Опять ошибка =("));
+
+            var fileLogger3 = new LocalFileLogger<bool>("text.txt");
+            fileLogger3.LogInfo("message7");
+            fileLogger3.LogInfo("message8");
+            fileLogger3.LogError("message9", new Exception("ABOBA"));
+
+        }
+    }
+    public interface ILogger
+    {
+        void LogInfo(string message);
+        void LogWarning(string message);
+        void LogError(string message, Exception ex);
+
+    }
+
+
+    public class LocalFileLogger<T>: ILogger { 
+        private string filePath;
+        public LocalFileLogger(string filePath)
+        {
+            this.filePath = filePath;
+        }
+        public void LogInfo(string message)
+        {
+            string GenericTypeName = typeof(T).Name;
+            File.AppendAllText(filePath, $"[Info] : [{GenericTypeName}] : {message}\n");
+        }
+
+        public void LogWarning(string message)
+        {
+            string GenericTypeName = typeof(T).Name;
+            File.AppendAllText(filePath, $"[Warning] : [{GenericTypeName}] : {message}\n");
+        }
+        public void LogError(string message, Exception ex)
+        {
+            string GenericTypeName = typeof(T).Name;
+            File.AppendAllText(filePath, $"[Error] : [{GenericTypeName}] : {message}. {ex.Message}\n");
+        }
+    }
+}
